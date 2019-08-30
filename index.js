cb376e86fa374bca5be83c469f2976db57e4e04e
tanfly <mcewen.stella@gmail.com>
2019-08-29 21:03:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair="*"){
+    return function (adjective="special"){
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
+
+const Calculator = {
+    add: function(a,b){
+        return a+b
+    },
+    subtract: function(a,b){
+        return a-b
+    },
+    multiply: function(a,b){
+        return a*b
+    },
+    divide: function(a,b){
+        return a/b
+    }
+}
+
+function actionApplyer(integer, array){
+    return array.reduce(function(result, fn){
+        return fn(result)
+    }, integer)
+}
