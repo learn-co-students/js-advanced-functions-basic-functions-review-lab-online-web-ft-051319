d4cbbbbc7b60cd6d18e38d9b73e9d461aa7b4e35
jenniferbusca <jennifer.busca@gmail.com>
2019-12-16 13:30:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,54 +1,36 @@
-// Your code here
-function saturdayFun(arg = 'roller-skate') {
-  return `This Saturday, I want to ${arg}!`
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`
 }
 
-function mondayWork(activity = "go to the office") {
+let mondayWork = function(activity="go to the office") {
   return `This Monday, I will ${activity}.`
 }
 
-function wrapAdjective(visualFlair = "*") {
-  return function(adjective = "special") {
-    return `You are ${visualFlair}${adjective}${visualFlair}!`
+let wrapAdjective = function(style="*") {
+  return function(adjective="special") {
+    return `You are ${style}${adjective}${style}!`
   }
 }
 
 const Calculator = {
-  add: function(a,b) {
+  add: (a,b) => {
     return a + b;
   },
-  subtract: function(a,b) {
-    return a - b
+  subtract: (a,b) => {
+    return a - b;
   },
-  multiply: function(a,b) {
-    return a * b
+  multiply: (a,b) => {
+    return a * b;
   },
-  divide: function(a,b) {
+  divide: (a,b) => {
     return a / b;
-  }
+  },
 }
-
-//
-// function actionApplyer(integer, array) {
-//   let transformedInt = integer
-//   if(array.length === 0) {
-//     return transformedInt
-//   }
-//   else {
-//     array.forEach(function(element) {
-//       let int = element(integer)
-//      transformedInt = int
-//     })
-//   }
-//   return transformedInt
-// }
 
 function actionApplyer(start, array) {
   let a = start
-
-  for (let i = 0; i < array.length; i++ ){
-    a = array[i](a)
-  }
-
+  array.forEach((fn) => {
+    a = fn(a)
+  })
   return a
 }
